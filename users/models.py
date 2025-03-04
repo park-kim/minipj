21496fb30c06c1fe40ba723d73f01091a093441c
rodzlen <qkqhrkxdkdy@naver.com>
2025-03-04 16:53:27 +0900

    :sparkles: create User model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,19 @@
+import uuid
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, max_length=100)
+    nickname = models.CharField(max_length=50, blank=True, null=True)
+    phone_number = models.CharField(max_length=30, blank=True, null=True)
+    last_login = models.DateTimeField(auto_now=True)
+    is_staff = models.BooleanField(default=False)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
+
+    def __str__(self):
+        return self.email
