ade273d7da1274dabcae96c518a9ab5938e90ce1
rodzlen <qkqhrkxdkdy@naver.com>
2025-03-04 17:08:39 +0900

    :bug: fix User model attr

--- users/models.py
+++ users/models.py
@@ -15,5 +15,16 @@ class User(AbstractUser):
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = ["username"]
 
+    groups = models.ManyToManyField(
+        "auth.Group",
+        related_name="custom_user_set",
+        blank=True,
+    )
+    user_permissions = models.ManyToManyField(
+        "auth.Permission",
+        related_name="custom_user_permissions",
+        blank=True,
+    )
+
     def __str__(self):
         return self.email
